e7538cc5f050ffc508e6671eb96aa309871e4414
pansy <526915701@qq.com>
2020-03-11 11:36:48 +0800

    init commit

--- src/main/java/com/spike/myshop/service/consumer/MyShopServiceUserConsumerApplication.java
+++ src/main/java/com/spike/myshop/service/consumer/MyShopServiceUserConsumerApplication.java
@@ -2,10 +2,11 @@ package com.spike.myshop.service.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
-@SpringBootApplication(scanBasePackages = "com.spike.myshop")
+@SpringBootApplication(scanBasePackages = "com.spike.myshop", exclude = DataSourceAutoConfiguration.class)
 @EnableHystrix
 @EnableHystrixDashboard
 public class MyShopServiceUserConsumerApplication {
--- src/main/java/com/spike/myshop/service/consumer/controller/TbUserController.java
+++ src/main/java/com/spike/myshop/service/consumer/controller/TbUserController.java
@@ -1,14 +1,16 @@
 package com.spike.myshop.service.consumer.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.github.pagehelper.PageInfo;
 import com.spike.myshop.commons.domain.TbUser;
 import com.spike.myshop.service.user.api.TbUserService;
+import com.spike.myshop.statics.backend.dto.DataTableDTO;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.List;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping(value = "user")
@@ -17,10 +19,42 @@ public class TbUserController {
     @Reference(version = "${services.versions.user.v1}")
     private TbUserService tbUserService;
 
+    /**
+     * 返回list页面
+     *
+     * @return
+     */
     @RequestMapping(value = "list", method = RequestMethod.GET)
-    public String list(Model model) {
-        List<TbUser> tbUsers = tbUserService.selectAll();
-        model.addAttribute("tbUsers", tbUsers);
+    public String list() {
         return "user/list";
+    }
+
+    /**
+     * 分页查询
+     *
+     * @param request request
+     * @param tbUser 查询条件
+     * @return 分页查询结果集
+     */
+    @ResponseBody
+    @RequestMapping(value = "page", method = RequestMethod.GET)
+    public DataTableDTO<TbUser> page(HttpServletRequest request, TbUser tbUser) {
+        String strDraw = request.getParameter("draw");
+        String strStart = request.getParameter("start");
+        String strLength = request.getParameter("length");
+
+        int draw = strDraw == null ? 0 : Integer.parseInt(strDraw);
+        int start = strStart == null ? 0 : Integer.parseInt(strStart);
+        int length = strLength == null ? 10 : Integer.parseInt(strLength);
+
+        // 封装 Datatables 需要的结果
+        PageInfo<TbUser> pageInfo = tbUserService.page(start, length, tbUser);
+        DataTableDTO<TbUser> dataTableDTO = new DataTableDTO<>();
+        dataTableDTO.setData(pageInfo.getList());
+        dataTableDTO.setDraw(draw);
+        dataTableDTO.setRecordsTotal(pageInfo.getTotal());
+        dataTableDTO.setRecordsFiltered(pageInfo.getTotal());
+
+        return dataTableDTO;
     }
 }
